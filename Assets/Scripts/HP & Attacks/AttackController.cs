1defec9b775e6323975ccd9861ebb957b5d625a3
Vasshell <81262716+Vasshell@users.noreply.github.com>
2025-11-01 18:55:26 +0300

    pre-merge

---
+++ Assets/Scripts/HP & Attacks/AttackController.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class AttackController : MonoBehaviour
+{
+
+    void Awake()
+    {
+
+    }
+    
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+
+    }
+}
