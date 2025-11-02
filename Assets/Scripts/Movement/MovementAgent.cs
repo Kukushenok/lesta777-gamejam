0a7889fb69dd3eb6578ff05428c7797abafaa627
MassterMax <maxmail877@gmail.com>
2025-11-02 18:31:47 +0300

    add simple sprites

--- Assets/Scripts/Movement/MovementAgent.cs
+++ Assets/Scripts/Movement/MovementAgent.cs
@@ -12,6 +12,8 @@ public class MovementAgent : MonoBehaviour
     {
         _rg = GetComponent<Rigidbody2D>();
         _rg.gravityScale = 0;
+        _rg.freezeRotation = true;
+        _rg.interpolation = RigidbodyInterpolation2D.Interpolate;
     }
     private void Update()
     {
