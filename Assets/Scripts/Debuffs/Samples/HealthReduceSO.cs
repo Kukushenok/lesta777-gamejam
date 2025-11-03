f5dea051d598e4ef5fea8ea0e06d93f33594a2ae
IlyaP <mrcoolmoder@gmail.com>
2025-11-03 18:09:05 +0300

    Max health is now working a little bit

--- Assets/Scripts/Debuffs/Samples/HealthReduceSO.cs
+++ Assets/Scripts/Debuffs/Samples/HealthReduceSO.cs
@@ -3,10 +3,12 @@
 [CreateAssetMenu(fileName = "HealthReduceDebuff", menuName = "Scriptable Objects/Debuffs/Health Reduce")]
 public class HealthReduceSO : DebuffSO
 {
-    [SerializeField] private float damage;
+    [SerializeField] private float modifier;
     public override void ApplyDebuff(GameObject player)
     {
         // this is shitty af. I guess i'll go this way. 
-        player.GetComponent<Health>().TakeDamage(damage);
+        var x = player.GetComponent<Health>();
+        x.maxHealth *= (1 - modifier);
+        x.health = Mathf.Clamp(x.health, 0, x.maxHealth);
     }
 }
--- Assets/Scripts/HP & Attacks/PlayerHealthUIDisplay.cs
+++ Assets/Scripts/HP & Attacks/PlayerHealthUIDisplay.cs
@@ -3,18 +3,25 @@
 public class PlayerHealthUIDisplay : MonoBehaviour
 {
     [SerializeField] private ValueDisplay<float> healthDisplay;
+    [SerializeField] private ValueDisplay<float> maxHealthDisplay;
+    private float wasMaxHP;
     private Health hp;
 
     private void Awake()
     {
         hp = FindFirstObjectByType<PlayerHealth>();
+        wasMaxHP = hp.maxHealth;
     }
 
     private void Update()
     {
         if (Mathf.Abs(hp.health - healthDisplay.DisplayValue) > 1e-5)
         {
-            healthDisplay.DisplayValue = hp.health / hp.maxHealth;
+            healthDisplay.DisplayValue = hp.health / wasMaxHP;
+        } 
+        if(Mathf.Abs(hp.maxHealth - healthDisplay.DisplayValue) > 1e-5)
+        {
+            maxHealthDisplay.DisplayValue = hp.maxHealth / wasMaxHP;
         }
     }
 }
